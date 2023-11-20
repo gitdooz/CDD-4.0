36fd8891a92d87611970ed2f663342b01608dafe
Oz Menezes <130259602+ozmenezes@users.noreply.github.com>
2023-11-19 23:25:20 -0300

    Update BancoDeDados.py

--- MEU PRIMEIRO CRUD/BancoDeDados.py
+++ MEU PRIMEIRO CRUD/BancoDeDados.py
@@ -1,22 +1,8 @@
 import mysql.connector
-
 banco = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="302302",
-    database="academiaturmad")
-
+    host='localhost',
+    user='root',
+    password ='134340',
+    database='academiadaberlinda'
+    )
 cursor = banco.cursor()
-pesquisa = 'select * from alunos;'
-cursor.execute(pesquisa)
-resultado = cursor.fetchall()
-
-for x in resultado:
-    print(x)
-
-banco.commit()
-#userid = meucursor.lastrowid
-#print(userid)
-cursor.close()
-banco.close()
-

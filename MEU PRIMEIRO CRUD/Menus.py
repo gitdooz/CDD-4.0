eb19b09fc09c6a7f19b80249c111998501ad0be5
Oz Menezes <130259602+ozmenezes@users.noreply.github.com>
2023-11-19 23:25:46 -0300

    Update Menus.py

--- MEU PRIMEIRO CRUD/Menus.py
+++ MEU PRIMEIRO CRUD/Menus.py
@@ -1,6 +1,9 @@
-from biblitoecaAcademia import *
+from bibliotecaAcademia import *
+from BancoDeDados import *
+import os
 
-while True:
+continuar = True
+while continuar:
     print('-----------  MENU -----------\n'
           '   > 1 - CADASTRAR ALUNO <\n'
           '   > 2 - CADASTRAR FUNCIONÁRIO <\n'
@@ -8,7 +11,7 @@ while True:
           '   > 4 - CADASTRAR PERSONAL <\n')
     primeiro_menu = int(input('O que deseja fazer?\n'
                               'DIGITE AQUI: '))
-
+    os.system('cls')
     if primeiro_menu == 1:
         print('-----------  MENU -----------\n'
               '   > 1 - INSERIR DADOS DO ALUNO <\n'
@@ -16,12 +19,23 @@ while True:
               '   > 3 - MOSTRAR DADOS <\n')
         opcao_aluno = int(input('O que deseja fazer?\n'
                                 'DIGITE AQUI: '))
+        os.system('cls')
         if opcao_aluno == 1:
             inserirAluno()
+            print('\nINFORMAÇÕES DO ALUNO\n')
+            nome = input('Nome: ')
+            cpf = input('CPF: ')
+            telefone = input('Telefone: ')
+            email = input('Email: ')
+            endereco = input('Endereço: ')
+
+            inserirAluno(nome, cpf, telefone, email, endereco)
         elif opcao_aluno == 2:
+            print('----------- DESLIGAMENTO DE ALUNO -----------')
             matricula = int(input('Digite a matrícula do aluno: '))
             deletarAluno(matricula)
         else:
+            print('\n----------- TABELA ALUNOS -----------')
             mostrarDadosAluno()
 
     if primeiro_menu == 2:
@@ -32,11 +46,18 @@ while True:
         opcao_funcionario = int(input('O que deseja fazer?\n'
                                       'DIGITE AQUI: '))
         if opcao_funcionario == 1:
-            inserirFuncionario()
+            print('----------- INFORMAÇÕES DO FUNCIONÁRIO -----------')
+            nome = input('Nome:')
+            cpf_funcionario = input('CPF: ')
+            departamento = int(input('Departamento: '))
+            salario = float(input('Salário: '))
+            inserirFuncionario(nome, cpf_funcionario, departamento,salario)
         elif opcao_funcionario == 2:
+            print('\n----------- DESLIGAMENTO DE FUNCIONÁRIO -----------')
             id_funcionario = int(input('Digite ID do funcionário: '))
             deletarFuncionario(id_funcionario)
         else:
+            print('----------- TABELA FUNCIONÁRIOS -----------')
             mostrarDadosFuncionario()
 
     if primeiro_menu == 3:
@@ -48,11 +69,14 @@ while True:
                                      'DIGITE AQUI: '))
 
         if opcao_modalidade == 1:
-            inserirModalidade()
+            print('----------- INFORMAÇÕES DA MODALIDADE -----------')
+            nome = input('Nome: ')
+            inserirModalidade(nome)
         elif opcao_modalidade == 2:
-            id_mod = int(input('Digite a matrícul do aluno: '))
+            id_mod = int(input('Digite a matrícula do aluno: '))
             deletarModalidade(id_mod)
         else:
+            print("----------- MODALIDADES -----------")
             mostrarDadosModalidade()
     if primeiro_menu == 4:
         print('-----------  MENU -----------\n'
@@ -62,10 +86,18 @@ while True:
         opcao_personal = int(input('O que deseja fazer?\n'
                                    'DIGITE AQUI: '))
         if opcao_personal == 1:
-            inserirPersonal()
+            print('----------- INFORMAÇÕES DO PERSONAL -----------')
+            cpf = input('CPF: ')
+            cref = input('CREF: ')
+            nome = input('Nome: ')
+            telefone = input('Telefone: ')
+            email = input('Email: ')
+            inserirPersonal(cpf, cref, nome, telefone, email)
         elif opcao_personal == 2:
-            cpf = int(input('Digite a matrícul do aluno: '))
+            print('----------- DELETAR PERSONAL -----------')
+            cpf = int(input('Digite a matrícula do aluno: '))
             deletarPersonal(cpf)
         else:
+            print('----------- TABELA PERSONAL -----------')
             mostrarDadosPersonal()
-
+        continuar = input('Deseja continuar? ')
